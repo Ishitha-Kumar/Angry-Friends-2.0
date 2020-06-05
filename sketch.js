48c2549efbe20ebb14b1827a6d6cbb7069aba667
Ishitha-Kumar <63245193+Ishitha-Kumar@users.noreply.github.com>
2020-06-05 09:12:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function setup(){
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig3 = new Pig(820, 210);
 
     log3 =  new Log(810,180,300, PI/2);
 
